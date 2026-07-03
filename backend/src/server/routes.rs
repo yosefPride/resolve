8d07506edf71a7367c513fde957add10ea835ded
yosef <yosefchaimpride@gmail.com>
2026-07-03 19:22:18 +0300

    Added auth/refresh route

--- backend/src/server/routes.rs
+++ backend/src/server/routes.rs
@@ -9,6 +9,7 @@ pub fn configure(config: &mut web::ServiceConfig) {
                 .route("/register", web::post().to(auth_handlers::register))
                 .route("/login", web::post().to(auth_handlers::login))
                 .route("/me", web::get().to(auth_handlers::me))
+                .route("/refresh", web::post().to(auth_handlers::refresh))
                 .route("/logout", web::post().to(auth_handlers::logout)),
         )
         .service(web::scope("/groups"))
