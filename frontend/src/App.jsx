7bc6d0f795fe1091943bde8bcba6e05139e68fe4
yosef <yosefchaimpride@gmail.com>
2026-07-04 04:03:27 +0300

    Added base footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-      </Routes>
-    </>
+    <div className='flex flex-col min-h-screen'> {/* Added min-h-screen for sticky footer */}
+      <div className='grow'>
+        <Header />
+        <Routes>
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
